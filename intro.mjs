fc093b08383014283d24b0363d02e544e07b52c7
Дід Alik <didalik.aim@gmail.com>
2025-12-20 14:40:44 +0000

    Improve UX

--- intro.mjs
+++ intro.mjs
@@ -1,3 +1,4 @@
-import { put, } from './lib/util.mjs'
+import { put, reset, } from './lib/util.mjs'
 
+reset({ content: document.getElementById('content1'), })
 put(`You requested ${location} on ${Date()} from YOUR_IP_ADDRESS`)
