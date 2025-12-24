d3cdf7cb71af848312c9fc69f1e99948232e01ce
Дід Alik <didalik.aim@gmail.com>
2025-12-24 22:52:09 +0000

    Add files

--- bwst.mjs
+++ bwst.mjs
@@ -1,4 +1,10 @@
 import { put, reset, } from './lib/util.mjs'
+import { connection, } from '../lib/util.mjs'
 
 reset({ content: document.getElementById('content1'), })
 put(`Delivered ${location} on ${Date()} to YOUR_IP_ADDRESS`, '<hr/>')
+  
+const ws = connection(new WebSocket(location.toString().replace('http', 'ws'))).
+  send("Hi there! I'm Ann.")
+
+setInterval(ws.open, 10000)                // auto-reconnect every 10s
