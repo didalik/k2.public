4507bd486d363fe28c6673e300927e2788e3cd5a
Дід Alik <didalik.aim@gmail.com>
2025-12-25 22:51:04 +0000

    Add Node JS WebSocket API

--- bwst.mjs
+++ bwst.mjs
@@ -5,6 +5,7 @@ reset({ content: document.getElementById('content1'), })
 put(`Delivered ${location} on ${Date()} to YOUR_IP_ADDRESS`, '<hr/>')
   
 const ws = connection(new WebSocket(location.toString().replace('http', 'ws'))).
+  on('error', err => console.error(err)).
   send("Hi there! I'm Ann.")
 
 setInterval(ws.open, 10000)                // auto-reconnect every 10s
