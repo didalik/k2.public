8ca61f5d92279c9b21ea93c17ed4ac7b8478bcb5
Дід Alik <didalik.aim@gmail.com>
2025-12-30 23:02:01 +0000

    Broadcast 1st message

--- bwst.mjs
+++ bwst.mjs
@@ -6,6 +6,7 @@ put(`Delivered ${location} on ${Date()} to YOUR_IP_ADDRESS`, '<hr/>')
   
 const ws = connection(new WebSocket(location.toString().replace('http', 'ws'))).
   on('error', err => console.error(err)).
+  on('message', console.log).
   send("Hi there! I'm Ann.")
 
 setInterval(ws.open, 10000)                // auto-reconnect every 10s
