8e7c92407f666609688cfc094cdb9002351d30bc
Дід Alik <didalik.aim@gmail.com>
2025-12-31 19:58:45 +0000

    Send 2 messages from each client

--- bwst.mjs
+++ bwst.mjs
@@ -1,12 +1,13 @@
 import { put, reset, } from './lib/util.mjs'
 import { connection, } from '../lib/util.mjs'
+import { wsclient_onmessage, } from '../local/lib/util.mjs'
 
 reset({ content: document.getElementById('content1'), })
 put(`Delivered ${location} on ${Date()} to YOUR_IP_ADDRESS`, '<hr/>')
   
 const ws = connection(new WebSocket(location.toString().replace('http', 'ws'))).
-  on('error', err => console.error(err)).
-  on('message', console.log).
+  on('error', console.error).
+  on('message', msg => wsclient_onmessage(ws, msg)).
   send("Hi there! I'm Ann.")
 
 setInterval(ws.open, 10000)                // auto-reconnect every 10s
