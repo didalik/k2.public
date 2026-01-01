54d291af85cf0c8d8b098e7318fd80f4b01cc88a
Дід Alik <didalik.aim@gmail.com>
2026-01-01 20:48:45 +0000

    Complete bwst backend

--- bwst.mjs
+++ bwst.mjs
@@ -1,13 +1,15 @@
 import { put, reset, } from './lib/util.mjs'
 import { connection, } from '../lib/util.mjs'
-import { wsclient_onmessage, } from '../local/lib/util.mjs'
+import { configuration, wsclient_onmessage, } from '../local/lib/util.mjs'
 
 reset({ content: document.getElementById('content1'), })
 put(`Delivered ${location} on ${Date()} to YOUR_IP_ADDRESS`, '<hr/>')
   
+configuration.me = 'Ann'
 const ws = connection(new WebSocket(location.toString().replace('http', 'ws'))).
   on('error', console.error).
   on('message', msg => wsclient_onmessage(ws, msg)).
+  on('close', data => console.log(configuration.me, 'close data', data)).
   send("Hi there! I'm Ann.")
 
 setInterval(ws.open, 10000)                // auto-reconnect every 10s
